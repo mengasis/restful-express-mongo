52232b9b5635ea6d3ffd82c8e940a1773973e98e
Manuel Venegas <mengasis@outlook.com>
2018-10-25 23:15:29 -0300

    Server started with http

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+import http from 'http'
 import express from 'express'
 import bodyParser from 'body-parser'
 
 const app = express()
+app.server = http.createServer(app)
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.listen(3000, () => {
-	console.log('Server is running!')
+const server = http.createServer(app)
+server.listen(3000)
+
+server.on('listening', () => {
+	const addr = server.address()
+	const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port
+
+	console.log(`Server is listening on ${bind}`)
 })
