2f08318bf5bfb16aee12afbf9c0a3afad5d6652d
Manuel Venegas <mengasis@outlook.com>
2018-10-25 23:06:31 -0300

    Initial Setup

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.listen(3000, () => {
+	console.log('Server is running!')
+})
