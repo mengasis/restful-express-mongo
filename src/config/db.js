5b636ec367fa3ed3baebdfe9c8608c4deeb16ceb
Manuel Venegas <mengasis@outlook.com>
2018-11-09 22:49:43 -0300

    [Fix] Connection error fixed

--- src/config/db.js
+++ src/config/db.js
@@ -6,12 +6,12 @@ Mongoose.Promise = global.Promise
 const connectToDb = async () => {
 	try {
 		await Mongoose.connect(
-			`mongodb://${config.DB.HOST}:27017/${config.DB.NAME}`,
+			`mongodb://mongo:27017/${config.DB.NAME}`,
 			{ useNewUrlParser: true }
 		)
 		console.log('Connected to mongo!!!')
 	} catch (err) {
-		console.log('Could not connect to MongoDB: ', err)
+		console.log(err)
 	}
 }
 
